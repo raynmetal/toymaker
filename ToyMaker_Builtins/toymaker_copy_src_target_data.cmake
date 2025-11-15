5dc7ea34b4c80a199edab632203d85fbe35ffcb8
Zoheb Shujauddin <zoheb2424@gmail.com>
2025-11-15 16:03:09 +0530

    makes it possible to build a game project from a local ToyMaker installation (as opposed to from ToyMaker's build tree)

--- ToyMaker_Builtins/toymaker_copy_src_target_data.cmake
+++ ToyMaker_Builtins/toymaker_copy_src_target_data.cmake
@@ -2,8 +2,6 @@
 function(toyMaker_copySrcTargetData srcTarget destTarget)
     add_custom_command(
         TARGET ${destTarget} POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory
-            "$<TARGET_PROPERTY:${srcTarget},BINARY_DIR>/data"
-            "$<TARGET_PROPERTY:${destTarget},BINARY_DIR>/data"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "$<IF:$<BOOL:$<TARGET_PROPERTY:${srcTarget},IMPORTED>>,$<PATH:REMOVE_FILENAME,$<TARGET_PROPERTY:${srcTarget},LOCATION>>toymaker,$<TARGET_PROPERTY:${srcTarget},BINARY_DIR>>/data" "$<TARGET_PROPERTY:${destTarget},BINARY_DIR>/data"
     )
 endfunction()
